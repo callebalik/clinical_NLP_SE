4a30e9f7e5137b9855076a3f7402e46d3f41b323
callebalik <carl.ollvik@gmail.com>
2021-08-02 02:09:39 +0200

    paths work in subprocess needed shell, careful

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+rom pathlib import Path
 import os
 import subprocess
 
@@ -10,10 +10,8 @@ file_path = Path(__file__).resolve()
 ROOT = file_path.parent.parent
 print(ROOT)
 DATA_PATH = ROOT / "data"
-NEG_PATH = DATA_PATH / "raw/negations"
 
 corpus_name = "conll2003_NE_built_in_layer"
-
 RAW_CORPUS_PATH = DATA_PATH / f"raw/corpus/{corpus_name}"
 OUTPUT_CORPUS_PATH = DATA_PATH / f"processed/corpus/{corpus_name}"
 
@@ -21,29 +19,11 @@ OUTPUT_CORPUS_PATH = DATA_PATH / f"processed/corpus/{corpus_name}"
 # use 100 sentences before splitting a file into a new doc
 # /home/callebalik/clinical_NLP_SE/data/processed/corpus/conll2003_NE_built_in_layer
 # /home/callebalik/clinical_NLP_SE/scripts/data/processed/corpus/conll2003_NE_built_in_layer/
-def convert_raw_to_spacy():
+def evaluate_model(model_name:str):
+    MODEL_PATH = ROOT / f"models/{model_name}"
+    output = f"--output {MODEL_PATH}/metrics.json"
+    displacy = f"--displacy-path {MODEL_PATH}"
 
-    # cmd = "
-    #     "python",
-    #     "-m",
-    #     "spacy",
-    #     "convert",
-    #     {RAW_CORPUS_PATH},
-    #     {OUTPUT_CORPUS_PATH},
-    #     "--converter",
-    #     "conll",
-    #     "--n-sents",
-    #     "100",
-    # "
-    cmd = f"python -m spacy convert {RAW_CORPUS_PATH}/ {OUTPUT_CORPUS_PATH}/ --converter conll --n-sents 100"
-    print(cmd)
+    cmd = f"python -m spacy evaluate" + f"{MODEL_PATH} {OUTPUT_CORPUS_PATH}" + output + displacy
     output = subprocess.call(f"{cmd}", shell=True)
     print(output)
-
-
-def main():
-    convert_raw_to_spacy()
-
-
-if __name__ == "__main__":
-    main()
